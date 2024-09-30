2d44e81b4d33ecbc70bb9749f4e8042d7a4428b8
diegodev9 <diegodev9@gmail.com>
2024-09-30 13:22:01 -0300

    added slug to users. added member show. added avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  after_commit :generate_slug
+
   PROFILE_TITLE = [
     "Senior Ruby on Rails Developer",
     "Full Stack Ruby on Rails Developer",
@@ -11,5 +13,34 @@ class User < ApplicationRecord
     "Junior Full Stack Ruby on Rails Developer",
     "Senior Java Developer",
     "Senior Front End Developer"
-].freeze
+  ].freeze
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def to_param
+    return nil unless persisted?
+    slug
+  end
+
+  def address
+    "#{city}, #{state}, #{country}, #{pincode}"
+  end
+
+  private
+
+  def generate_slug
+    slug = [ id, first_name, last_name ].join("-")
+    return if self.slug == slug
+    self.update(slug:)
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[city country]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
