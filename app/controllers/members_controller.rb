f2af789c93d633f3a061eed2b5a2c4b51a5cff30
diegodev9 <diegodev9@gmail.com>
2024-10-02 08:59:13 -0300

    added stimulus controller for bootstrap modal

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,15 @@
 class MembersController < ApplicationController
+  before_action :set_user, only: %i[show edit_description]
+
   def show
+  end
+
+  def edit_description
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: "home#index"
 
 
-  get "members/:id", to: "members#show", as: 'member'
+  get "members/:id", to: "members#show", as: "member"
+  get "edit_description/:id", to: "members#edit_description", as: "edit_member_description"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
