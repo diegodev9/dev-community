b9f1ea51315368ae9efefb3c1fb121f8cd75b116
diegodev9 <diegodev9@gmail.com>
2024-10-14 08:49:02 -0300

    added update user description with turbo stream

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-  before_action :set_user, only: %i[show edit_description]
+  before_action :set_user, only: %i[show edit_description update_description]
 
   def show
   end
@@ -7,9 +7,21 @@ class MembersController < ApplicationController
   def edit_description
   end
 
+  def update_description
+    respond_to do |format|
+      if @user.update(user_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("member_description", partial: "members/member_description", locals: { user: @user }) }
+      end
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:about)
   end
 end
